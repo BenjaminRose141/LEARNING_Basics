72f2fb3834061bf2d9caad38e62a67cec14410b3
Benjamin Rose <rose.benjamin@t-online.de>
2020-01-19 01:19:46 +0100

    Finished Clock exercise

---
+++ Learning_Basics/Assets/Scripts/Clock.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField] Transform hoursTransform, minutesTransform, secondsTransform;
+    [SerializeField] bool continuous;
+
+    private const float degreesPerHour = 30,
+                        degreesPerMinute = 6f,
+                        degreesPerSecond = 6f;
+
+
+    private void Update()
+    {
+        if(continuous)
+        {
+            UpdateContinuous();
+        }
+        else
+        {
+            UpdateDiscrete();
+        }
+    }
+
+    private void UpdateContinuous()
+    {
+        TimeSpan timeSpan = DateTime.Now.TimeOfDay;
+
+        hoursTransform.localRotation = Quaternion.Euler(0, (float) timeSpan.TotalHours * degreesPerHour, 0);
+        minutesTransform.localRotation = Quaternion.Euler(0, (float) timeSpan.TotalMinutes * degreesPerMinute, 0);
+        secondsTransform.localRotation = Quaternion.Euler(0, (float) timeSpan.TotalSeconds * degreesPerSecond, 0);
+    }
+
+    private void UpdateDiscrete()
+    {
+        DateTime dateTime = DateTime.Now;
+
+        hoursTransform.localRotation = Quaternion.Euler(0, dateTime.Hour * degreesPerHour, 0);
+        minutesTransform.localRotation = Quaternion.Euler(0, dateTime.Minute * degreesPerMinute, 0);
+        secondsTransform.localRotation = Quaternion.Euler(0, dateTime.Second * degreesPerSecond, 0);
+    }
+}
